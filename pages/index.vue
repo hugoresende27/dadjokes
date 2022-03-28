a4eae1c291f97166a5ee77ca75f1f336ecfda853
hugoresende27 <hugoresende27@gmail.com>
2022-03-28 23:47:30 +0100

    1

--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,24 @@
 <template>
-  <Tutorial/>
+  <!-- <Tutorial/> -->
+
+  <h2>Welcome to dadJokes</h2>
 </template>
 
 <script>
 export default {
-  name: 'IndexPage'
+  name: 'IndexPage',
+
+  head() {
+        return {
+            title:'Welcome to dad jokes',
+            meta: [
+                {
+                    hid: 'description',
+                    name:'description',
+                    content:'Best place for a corny dad jokes'
+                }
+            ]
+        }
+    }
 }
 </script>
